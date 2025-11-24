5db9d71defed683a7fc025dfde73c4361fbd5c9d
Sven Erbach <sven.erbach@protonmail.com>
2025-11-24 14:23:44 +0100

    add layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
+import BaseLayout from './BaseLayout';
 import SplashScreen from './Splashscreen';
 
 function App() {
   return (
-    <>
+    <BaseLayout>
       <SplashScreen />
-    </>
+    </BaseLayout>
   );
 }
 
---
+++ src/BaseLayout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export default function BaseLayout({ children }: LayoutProps) {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <article>{children}</article>
+    </main>
+  );
+}
