1e834ceac8eba7c4b8cbee6c8da1a2e1c84db06c
Sasa <sasa.trajkova@gmail.com>
2025-11-25 15:16:30 +0100

    load waldo's picture on find waldo screen

--- src/routes/findWaldo.tsx
+++ src/routes/findWaldo.tsx
@@ -1,6 +1,8 @@
+import { useEffect, useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { createFileRoute, Link } from '@tanstack/react-router';
 import { Scanner, type IDetectedBarcode } from '@yudiel/react-qr-scanner';
+import { getUser, User } from '@/database/database';
 
 export const Route = createFileRoute('/findWaldo')({
   component: RouteComponent,
@@ -9,25 +11,49 @@ export const Route = createFileRoute('/findWaldo')({
   }),
 });
 
+type FindWaldoComponentProps = {
+  profile: User;
+};
+
+function FindWaldoComponent({ profile }: FindWaldoComponentProps) {
+  return (
+    <>
+      <div className="h-dvh w-full flex flex-col">
+        <div className="w-full flex-1 bg-white flex h-50">
+          <img src={profile.imageBase64} className="object-cover flex-1" />
+        </div>
+        <div className="w-full flex-1 bg-black h-50">
+          <Scanner onScan={(result) => handleScanResult(result)} />
+        </div>
+      </div>
+    </>
+  );
+}
+
 function RouteComponent() {
   const { id } = Route.useSearch();
+  const [profile, setProfile] = useState<User | null>(null);
 
   function handleScanResult(result: IDetectedBarcode[]) {
     console.log(result);
   }
 
+  useEffect(() => {
+    const loadUser = async () => {
+      const user = await getUser(id);
+      setProfile(user);
+    };
+    loadUser();
+  }, [id]);
+
+  if (!profile) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
       <div className="h-dvh w-full flex flex-col">
-        <div className="w-full flex-1 bg-white flex h-50">
-          <img
-            src="https://picsum.photos/300"
-            className="object-cover flex-1"
-          />
-        </div>
-        <div className="w-full flex-1 bg-black h-50">
-          <Scanner onScan={(result) => handleScanResult(result)} />
-        </div>
+        <FindWaldoComponent profile={profile} />
 
         <div className="absolute flex gap-2">
           <Link to="/askWaldo" search={{ id }}>
