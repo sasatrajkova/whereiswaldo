a9d3c851885d170ab968fd59e119618213ef1e3f
Sven Erbach <sven.erbach@protonmail.com>
2025-11-25 11:02:39 +0100

    how: add skeleton

--- src/routes/askWaldo.tsx
+++ src/routes/askWaldo.tsx
@@ -1,16 +1,43 @@
 import { Button } from '@/components/ui/button';
-import { createFileRoute, Link } from '@tanstack/react-router';
+import { Input } from '@/components/ui/input';
+import { createFileRoute, useNavigate } from '@tanstack/react-router';
+import { useState, type FormEvent } from 'react';
 
 export const Route = createFileRoute('/askWaldo')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const [answer, setAnswer] = useState<string>('');
+  const navigate = useNavigate();
+
+  function handleFormSubmit(e: FormEvent) {
+    e.preventDefault();
+    console.log(answer.trim());
+
+    setAnswer('');
+
+    navigate({ to: '/myWaldos' });
+  }
+
   return (
     <>
-      <Link to="/myWaldos">
-        <Button>My Waldos</Button>
-      </Link>
+      <form
+        onSubmit={(e) => handleFormSubmit(e)}
+        className="flex flex-col gap-3 w-full px-8"
+      >
+        <img src="https://picsum.photos/200" className="object-cover" />
+        <h1>What is your name?</h1>
+        <Input
+          type="text"
+          placeholder="Add your answer in here"
+          value={answer}
+          onChange={(e) => setAnswer(e.target.value)}
+        />
+        <Button type="submit" disabled={!answer} className="w-full">
+          Submit
+        </Button>
+      </form>
     </>
   );
 }
