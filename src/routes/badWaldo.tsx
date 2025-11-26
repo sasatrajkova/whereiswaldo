eab5d3995db31fb256caa8c20f7a4c5061f37323
Sven Erbach <sven.erbach@protonmail.com>
2025-11-26 08:55:54 +0100

    add funny title

--- src/routes/badWaldo.tsx
+++ src/routes/badWaldo.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, Link, useNavigate } from '@tanstack/react-router';
+import { createFileRoute, Link } from '@tanstack/react-router';
 import { Button } from '@/components/ui/button';
 import badWaldo from '@/assets/WrongQRCodeReaction.webp';
 
@@ -11,18 +11,39 @@ export const Route = createFileRoute('/badWaldo')({
 
 function RouteComponent() {
   const { id } = Route.useSearch();
-
   return (
     <>
-      <div className="h-dvh w-full flex flex-col">
-        <div className="h-dvh w-full flex flex-col">
-          <div className="w-full flex-1 bg-white flex h-50">
-            <img src={badWaldo} className="object-cover flex-1" />
-          </div>
-        </div>
-        <div className="absolute flex gap-2">
-          <Link to="/findWaldo" search={{ id }}>
-            <Button type="button">Try again</Button>
+      <div>
+        <div className="relative w-full h-full p-8 flex flex-col gap-2">
+          <img
+            src={badWaldo}
+            className="object-cover flex-1 h-full animate-fade-in rounded-xl"
+          />
+          <h2 className="absolute inset-0 flex self-center justify-center text-red-600 font-bold text-5xl animate-zoom-in translate-y-[-150px]">
+            <span className="absolute top-[12px] text-red-200 scale-[95%]">
+              WRONG WALDO!!
+            </span>
+            <span className="absolute top-[10px] text-red-300 scale-[96%]">
+              WRONG WALDO!!
+            </span>
+            <span className="absolute top-[8px] text-red-300 scale-[97%]">
+              WRONG WALDO!!
+            </span>
+            <span className="absolute top-[6px] text-red-400 scale-[98%]">
+              WRONG WALDO!!
+            </span>
+            <span className="absolute top-[4px] text-red-400 scale-[99%]">
+              WRONG WALDO!!
+            </span>
+            <span className="absolute top-[2px] text-red-400 scale-[99%]">
+              WRONG WALDO!!
+            </span>
+            <span className="absolute top-0">WRONG WALDO!!</span>
+          </h2>
+          <Link to="/findWaldo" search={{ id }} className="w-full">
+            <Button type="button" className="w-full">
+              Try again
+            </Button>
           </Link>
         </div>
       </div>
