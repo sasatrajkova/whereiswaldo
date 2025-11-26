1744fdc7b35257b932e668f8a4807f6007acfff5
Sven Erbach <sven.erbach@protonmail.com>
2025-11-26 13:16:28 +0100

    fix button width

--- src/routes/exploreWaldo.tsx
+++ src/routes/exploreWaldo.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button';
 import { getAnswersForUser, getUser, User } from '@/database/database';
 import { createFileRoute, Link } from '@tanstack/react-router';
 import { useState, useEffect } from 'react';
-import { WordCloud } from "@isoterik/react-word-cloud";
+import { WordCloud } from '@isoterik/react-word-cloud';
 
 export const Route = createFileRoute('/exploreWaldo')({
   component: RouteComponent,
@@ -14,22 +14,22 @@ export const Route = createFileRoute('/exploreWaldo')({
 function RouteComponent() {
   const { id } = Route.useSearch();
   const [waldo, setWaldo] = useState<User>();
-  const [words, setWords] = useState<{ text: string, value: number }[]>([])
+  const [words, setWords] = useState<{ text: string; value: number }[]>([]);
 
   useEffect(() => {
     getUser(id as string).then((response) =>
-          response ? setWaldo(response) : null
-        );
+      response ? setWaldo(response) : null
+    );
     getAnswersForUser(id as string).then((res) => setWords(res));
   }, []);
 
   return (
     <>
-      <h1>{ waldo?.name }</h1>
+      <h1>{waldo?.name}</h1>
       <img src={waldo?.imageBase64} />
       <WordCloud words={words} width={300} height={200} />
-      <Link to="/chooseWaldo">
-        <Button className="w-100">Find the next Waldo</Button>
+      <Link to="/chooseWaldo" className="w-full">
+        <Button className="w-full">Find the next Waldo</Button>
       </Link>
     </>
   );
