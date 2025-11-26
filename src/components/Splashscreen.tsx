bf80e1fd1d0cb9f865f89ee6c1e151010b79888a
Sven Erbach <sven.erbach@protonmail.com>
2025-11-26 10:23:41 +0100

    styling

--- src/components/Splashscreen.tsx
+++ src/components/Splashscreen.tsx
@@ -4,30 +4,41 @@ import { getMe, User } from '@/database/database';
 import { useEffect, useState } from 'react';
 
 export default function SplashScreen() {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [user, setUser] = useState<User>();
   useEffect(() => {
     getMe().then((value) => {
       if (value) {
         setUser(value);
       }
+
+      setIsLoading(false);
     });
   }, []);
+
+  if (isLoading) {
+    return <span className="font-mono">Thinking...</span>;
+  }
+
   return (
     <>
       {user && (
         <>
-          <h1>{'Welcome back' + user.name + '!'}</h1>
-          <img className="w-60 rounded-xl" src={user.imageBase64} />
-          <Link to="/chooseWaldo">
-            <Button>Continue the adventure</Button>
+          <h1>{'Welcome back ' + user.name + '!'}</h1>
+          <img
+            className="w-60 h-60 object-cover rounded-xl mb-8"
+            src={user.imageBase64}
+          />
+          <Link to="/chooseWaldo" className="w-full">
+            <Button className="w-full">Continue the adventure</Button>
           </Link>
         </>
       )}
       {!user && (
         <>
           <h1>{"Where's Waldo?"}</h1>
-          <Link to="/createAvatar">
-            <Button>Start the adventure</Button>
+          <Link to="/createAvatar" className="w-full">
+            <Button className="w-full">Start the adventure</Button>
           </Link>
           <p className="text-lg sm:text-xl text-muted-foreground mb-12 max-w-md mx-auto leading-relaxed">
             {'Embark on a jungle adventure and find Waldo hidden in the wild!'}
