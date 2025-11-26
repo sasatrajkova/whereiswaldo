6f1381149d0874539baa89ecf81a190e03adf680
Sven Erbach <sven.erbach@protonmail.com>
2025-11-26 11:06:42 +0100

    update nav button color

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -17,7 +17,7 @@ const buttonVariants = cva(
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost:
-          'hover:bg-accent hover:text-primary-foreground dark:hover:bg-primary/50',
+          'bg-background hover:bg-accent hover:text-primary-foreground dark:hover:bg-primary/50',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
